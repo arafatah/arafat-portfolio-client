c384205801d642bdafb59d69de5e10f12a4281f0
arafatah <arafathowlader456@gmail.com>
2023-12-13 16:03:10 +0600

    send message to mail added

--- src/ContactSection/ContactForm.jsx
+++ src/ContactSection/ContactForm.jsx
@@ -1,51 +1,73 @@
-import { AiFillPhone, AiOutlineMail } from "react-icons/ai";
 import { useForm, FormProvider } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
+import { useRef } from "react";
 
 const ContactForm = () => {
-    const methods = useForm();
-  
-    const onSubmit = (data) => {
-      // Handle the form data (e.g., send it to your server or an email service)
-      console.log(data);
-    };
-  
-    return (
-        <FormProvider {...methods}>
+  const methods = useForm();
+  const form = useRef();
+
+  const onSubmit = (data) => {
+    emailjs
+      .sendForm(
+        "service_0dacs08",
+        "template_37lt2md",
+        form.current,
+        "cQ7YEuOE5LdAuejD6"
+      )
+      .then(
+        (result) => {
+          Swal.fire({
+            icon: "success",
+            title: "Thank you!",
+            text: "Your message has been sent successfully!",
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    console.log(data);
+  };
+
+  return (
+    <FormProvider {...methods}>
       <form
+        ref={form}
         onSubmit={methods.handleSubmit(onSubmit)}
         className="flex flex-col gap-4 w-full md:p-6 rounded-md"
       >
         <input
           type="text"
           placeholder="Name"
-          {...methods.register('name', { required: 'Name is required' })}
+          {...methods.register("name", { required: "Name is required" })}
           className="p-2  rounded-md w-full bg-gray-700 text-white"
         />
         <input
           type="email"
           placeholder="Email"
-          {...methods.register('email', { required: 'Email is required' })}
+          {...methods.register("email", { required: "Email is required" })}
           className="p-2  rounded-md w-full bg-gray-700 text-white"
         />
         <textarea
           placeholder="Message"
-          {...methods.register('message', { required: 'Message is required' })}
+          {...methods.register("message", { required: "Message is required" })}
           rows="4"
           className="p-2  rounded-md w-full bg-gray-700 text-white"
         />
         <div className="flex justify-end">
-        <button
-          type="submit"
-          className="bg-blue-500 text-white p-3 rounded-md w-1/2  hover:bg-blue-700 transition-colors"
-        >
-          Send Message
-        </button>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-3 rounded-md w-1/2  hover:bg-blue-700 transition-colors"
+          >
+            Send Message
+          </button>
         </div>
       </form>
     </FormProvider>
-      
-    );
-  };
-  
+  );
+};
 
-  export default ContactForm;
+export default ContactForm;
